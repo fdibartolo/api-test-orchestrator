40783ec5a9612c668b7860c67d1bc9e4eaaff6d5
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2026-09-16 18:39:40 -0300

    reorder imports

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
-load_dotenv()
-
 from fastapi import FastAPI
 from src.routes import router
+
+load_dotenv()
 
 app = FastAPI(title="APITest Orchestrator Python Middleware")
 app.include_router(router)
